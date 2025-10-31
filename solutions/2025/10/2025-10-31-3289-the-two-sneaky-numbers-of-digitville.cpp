f8f6f9f1892e8bc1b2be394b707a8df0df9e4c1b
Danielyan-Hovo <96976763+Danielyan-Hovo@users.noreply.github.com>
2025-10-31 09:25:35 +0400

    Add CPP solution for Problem 3289: The Two Sneaky Numbers of Digitville (Easy)

---
+++ solutions/2025/10/2025-10-31-3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+
+        vector<int> result;
+
+        int frequency[100] = {0};
+
+        for (int number : nums) {
+
+            frequency[number]++;
+
+            if (frequency[number] == 2) {
+                result.push_back(number);
+            }
+        }
+
+        return result;
+    }
+};
