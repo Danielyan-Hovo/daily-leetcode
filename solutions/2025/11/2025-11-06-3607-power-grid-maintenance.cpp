943df5f77593635b290d1913d0965e291d3c0702
Danielyan-Hovo <96976763+Danielyan-Hovo@users.noreply.github.com>
2025-11-06 06:30:40 +0400

    Add CPP solution for Problem 3607: Power Grid Maintenance (Medium)

---
+++ solutions/2025/11/2025-11-06-3607-power-grid-maintenance.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <vector>
+int binary_search(std::vector<int> arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+    int mid = left + (right - left) / 2;
+
+    if (arr.at(mid) == target) return mid;
+    if (arr.at(mid) < target) {
+
+        left = mid + 1;
+    } else {
+
+        right = mid - 1;
+    }
+}
+return -1;
+}
+template<typename T>
+std::vector<T> get_words() {
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream ss{line};
+    ss >> std::boolalpha;
+    std::vector<T> v;
+    std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+    return v;
+}
+void ignore_line() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+int main() {
+    std::vector<int> arr = get_words<int>();
+    int target;
+    std::cin >> target;
+    ignore_line();
+    int res = binary_search(arr, target);
+    std::cout << res << '\n';
+}
