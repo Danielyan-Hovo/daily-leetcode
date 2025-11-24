176788213d57359118c42ff18ce2f18c5075df3a
Danielyan-Hovo <96976763+Danielyan-Hovo@users.noreply.github.com>
2025-11-24 16:56:27 +0400

    Add CPP solution for Problem 1018: Binary Prefix Divisible By 5 (Easy)

---
+++ solutions/2025/11/2025-11-24-1018-binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        vector<bool> result;
+        int currentRemainder = 0;
+
+        for (int binaryDigit : nums) {
+
+            currentRemainder = ((currentRemainder << 1) | binaryDigit) % 5;
+
+            result.push_back(currentRemainder == 0);
+        }
+
+        return result;
+    }
+};
